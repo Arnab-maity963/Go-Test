775427d28cc129bf5f0b007b442087604b902343
Arnab Kumar Maity <arnabmaity71379@gmail.com>
2026-09-12 09:01:20 +0530

    Fantastic project added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.26.5
+
+require github.com/Rajdeep-Nemo/sleepycat v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	slp "github.com/Rajdeep-Nemo/sleepycat"
+)
+
+func main() {
+	age, err := slp.Int(
+		slp.Prompt("Enter age: "),
+		slp.MaxAttempt(0),
+	)
+	if err != nil {
+		fmt.Println("You die....");
+	} else {
+		fmt.Println("Your age is ",age);
+	}
+}
